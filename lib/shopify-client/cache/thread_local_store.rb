dcd894d9cb9ece2a698492997207c72ce9e7d243
Kelsey Judson <k+github@judson.nz>
2021-06-24 04:00:10 +1200

    Partial ThreadLocalStore

--- lib/shopify-client/cache/thread_local_store.rb
+++ lib/shopify-client/cache/thread_local_store.rb
@@ -4,6 +4,13 @@ module ShopifyClient
   module Cache
     class ThreadLocalStore < Store
       # TODO
+
+      # @param key [String]
+      #
+      # @return [String]
+      private def build_expiry_key(key)
+        "#{key}:expires_at"
+      end
     end
   end
 end
