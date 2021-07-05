145ddcd76f88709b7061599fd2270a96396055ce
Kelsey Judson <k+github@judson.nz>
2021-07-05 15:49:45 +1200

    Expand on comment

--- spec/integration/throttling_spec.rb
+++ spec/integration/throttling_spec.rb
@@ -2,6 +2,11 @@
 
 module ShopifyClient
   RSpec.describe 'Throttling' do
-    # TODO: Test that many quick requests in succession are throttled, and do not trigger a 429 response.
+    # TODO
+    #
+    # Test that many quick requests in succession are throttled, and do not
+    # trigger a 429 response. This could be tricky due to the 429 retry
+    # middleware, and I'm not really keen on making that configurable just for
+    # testing purposes.
   end
 end
